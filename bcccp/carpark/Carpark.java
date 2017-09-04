42c996d8f23992151d64b5d453df3264e62904fd
smrtmlb <30581472+smrtmlb@users.noreply.github.com>
2017-09-04 11:30:41 +1000

    Formatted, Organized
    Formatted, Organized

--- bcccp/carpark/Carpark.java
+++ bcccp/carpark/Carpark.java
@@ -8,20 +8,16 @@ import bcccp.tickets.season.ISeasonTicket;
 import bcccp.tickets.season.ISeasonTicketDAO;
 
 public class Carpark implements ICarpark {
-	
+
 	private List<ICarparkObserver> observers;
 	private String carparkId;
 	private int capacity;
 	private int numberOfCarsParked;
 	private IAdhocTicketDAO adhocTicketDAO;
 	private ISeasonTicketDAO seasonTicketDAO;
-	
-	
-	
-	public Carpark(String name, int capacity, 
-			IAdhocTicketDAO adhocTicketDAO, 
-			ISeasonTicketDAO seasonTicketDAO) {
-		//Initializing constructor variables.
+
+	public Carpark(String name, int capacity, IAdhocTicketDAO adhocTicketDAO, ISeasonTicketDAO seasonTicketDAO) {
+		// Initializing constructor variables.
 		this.carparkId = name;
 		this.capacity = capacity;
 		this.adhocTicketDAO = adhocTicketDAO;
@@ -29,30 +25,22 @@ public class Carpark implements ICarpark {
 
 	}
 
-
-
 	@Override
 	public void register(ICarparkObserver observer) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public void deregister(ICarparkObserver observer) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public String getName() {
 		return carparkId;
 	}
-
-
 
 	@Override
 	public boolean isFull() {
@@ -60,23 +48,17 @@ public class Carpark implements ICarpark {
 		return false;
 	}
 
-
-
 	@Override
 	public IAdhocTicket issueAdhocTicket() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
-
 	@Override
 	public void recordAdhocTicketEntry() {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public IAdhocTicket getAdhocTicket(String barcode) {
@@ -84,39 +66,29 @@ public class Carpark implements ICarpark {
 		return null;
 	}
 
-
-
 	@Override
 	public float calculateAddHocTicketCharge(long entryDateTime) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-
-
 	@Override
 	public void recordAdhocTicketExit() {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public void registerSeasonTicket(ISeasonTicket seasonTicket) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public void deregisterSeasonTicket(ISeasonTicket seasonTicket) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public boolean isSeasonTicketValid(String ticketId) {
@@ -124,31 +96,22 @@ public class Carpark implements ICarpark {
 		return false;
 	}
 
-
-
 	@Override
 	public boolean isSeasonTicketInUse(String ticketId) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-
-
 	@Override
 	public void recordSeasonTicketEntry(String ticketId) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 	@Override
 	public void recordSeasonTicketExit(String ticketId) {
 		// TODO Auto-generated method stub
-		
-	}
 
-	
-	
+	}
 
 }
