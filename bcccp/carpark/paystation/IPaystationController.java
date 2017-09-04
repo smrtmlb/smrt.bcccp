d28a2d2bd439c4adab649d601c64bd8c7af42599
smrtmlb <30581472+smrtmlb@users.noreply.github.com>
2017-09-04 11:32:31 +1000

    Formatted, Organized
    Formatted, Organized

--- bcccp/carpark/paystation/IPaystationController.java
+++ bcccp/carpark/paystation/IPaystationController.java
@@ -2,9 +2,11 @@ package bcccp.carpark.paystation;
 
 //Interface pay station controller
 public interface IPaystationController {
-	
+
 	public void ticketInserted(String barcode);
+
 	public void ticketPaid();
+
 	public void ticketTaken();
 
 }
