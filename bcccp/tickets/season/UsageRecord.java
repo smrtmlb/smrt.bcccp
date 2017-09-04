be0c7d7f6d6935e7ccb9adbd5dff0462d2e67101
smrtmlb <30581472+smrtmlb@users.noreply.github.com>
2017-09-04 11:34:39 +1000

    Formatted, Organized
    Formatted, Organized

--- bcccp/tickets/season/UsageRecord.java
+++ bcccp/tickets/season/UsageRecord.java
@@ -1,47 +1,35 @@
 package bcccp.tickets.season;
 
 public class UsageRecord implements IUsageRecord {
-	
+
 	String ticketId;
 	long startDateTime;
 	long endDateTime;
-	
-	
-	
+
 	public UsageRecord(String ticketId, long startDateTime) {
-		//Initializing constructor variables.
+		// Initializing constructor variables.
 		this.ticketId = ticketId;
 		this.startDateTime = startDateTime;
 	}
-
-
 
 	@Override
 	public void finalise(long endDateTime) {
 		this.endDateTime = endDateTime;
 	}
 
-
-
 	@Override
 	public long getStartTime() {
 		return startDateTime;
 	}
-
-
 
 	@Override
 	public long getEndTime() {
 		return endDateTime;
 	}
 
-
-
 	@Override
 	public String getSeasonTicketId() {
 		return ticketId;
 	}
-	
-	
-	
+
 }
