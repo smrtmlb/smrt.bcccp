2cdfa0f61024e499775a1dbefd182a4415c35989
smrtmlb <30581472+smrtmlb@users.noreply.github.com>
2017-09-04 11:34:03 +1000

    Formatted, Organized
    Formatted, Organized

--- bcccp/tickets/season/ISeasonTicketDAO.java
+++ bcccp/tickets/season/ISeasonTicketDAO.java
@@ -2,12 +2,16 @@ package bcccp.tickets.season;
 
 // Interface season ticket dao.
 public interface ISeasonTicketDAO {
-	
+
 	public void registerTicket(ISeasonTicket ticket);
+
 	public void deregisterTicket(ISeasonTicket ticket);
+
 	public int getNumberOfTickets();
-	
+
 	public ISeasonTicket findTicketById(String ticketId);
-	public void recordTicketEntry(String ticketId );
+
+	public void recordTicketEntry(String ticketId);
+
 	public void recordTicketExit(String ticketId);
 }
