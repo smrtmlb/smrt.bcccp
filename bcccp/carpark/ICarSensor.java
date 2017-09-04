9314a29bcac804c59d89aa651923dffdaa054513
smrtmlb <30581472+smrtmlb@users.noreply.github.com>
2017-09-04 11:32:08 +1000

    Formatted, Organized
    Formatted, Organized

--- bcccp/carpark/ICarSensor.java
+++ bcccp/carpark/ICarSensor.java
@@ -3,9 +3,11 @@ package bcccp.carpark;
 // Interface car sensor
 public interface ICarSensor {
 	public void registerResponder(ICarSensorResponder responder);
+
 	public void deregisterResponder(ICarSensorResponder responder);
-	
+
 	public String getId();
+
 	public boolean carIsDetected();
 
 }
