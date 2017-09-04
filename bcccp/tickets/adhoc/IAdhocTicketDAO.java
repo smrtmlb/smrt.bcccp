8ab0b980dcf00291e06deea5f4ede98d5efa91e5
smrtmlb <30581472+smrtmlb@users.noreply.github.com>
2017-09-04 11:33:29 +1000

    Formatted, Organized
    Formatted, Organized

--- bcccp/tickets/adhoc/IAdhocTicketDAO.java
+++ bcccp/tickets/adhoc/IAdhocTicketDAO.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 // Interface adhoc ticket dao
 public interface IAdhocTicketDAO {
-	
-	public IAdhocTicket createTicket(String carparkId);
-	public IAdhocTicket findTicketByBarcode(String barcode);
-	public List<IAdhocTicket> getCurrentTickets();
 
+	public IAdhocTicket createTicket(String carparkId);
+
+	public IAdhocTicket findTicketByBarcode(String barcode);
+
+	public List<IAdhocTicket> getCurrentTickets();
 
 }
