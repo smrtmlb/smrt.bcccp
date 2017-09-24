3827d6447ec45e4f98f5fa2091cdc83fe8ea8a91
Sankaja <sugeeg@gmail.com>
2017-09-24 20:19:42 +1000

    final
    FInal Adhoc Ticket

--- test/tickets/adhoc/TestAdhocTicketDAO.java
+++ test/tickets/adhoc/TestAdhocTicketDAO.java
@@ -18,4 +18,21 @@ public class TestAdhocTicketDAO {
 		adhocTicketFactory_ = mock(iadhocTicketFactory_.class);
 		currentTickets = new HashMap<>();
 	}
+	
+		@After
+	public void testAdhocTicketDAO() throws Exception {
+		iadhocTicketFactory_ = null;
+		
+	}
+	
+	
+	public Int testisCurrent() {
+		IAdhocTicket ticket = adhocTicketFactory_.make(carparkId, ++currentTicketNo);
+		currentTickets.put(ticket.getBarcode(), ticket);
+		assertTrue(iticket instanceof iticket);
+		assertEquals(iticket.getName(), ticket);
+	
+	}
+
+
 }
