880878ff6f74c05d84460039eeb448b8348290d4
Sankaja <sugeeg@gmail.com>
2017-09-24 20:06:51 +1000

    Final one
    Test adhoc final

--- test/tickets/adhoc/TestAdhocTicket.java
+++ test/tickets/adhoc/TestAdhocTicket.java
@@ -29,6 +29,7 @@ public class TestAdhocTicket {
 	private IcarparkId carparkId;
 	private iticketNo ticketNo;
 	private Ibarcode barcode;
+	private ientryDateTime entryDateTime;
 	private ISeasonTicketDAO seasonTicketDAO;
 	private Istate state;
 	
@@ -36,7 +37,7 @@ public class TestAdhocTicket {
 	private IAdhocTicket iAdhocTicket;
 	private ISeasonTicket iSeasonTicket;
 	
-		@Before
+	@Before
 	public void setUp() {
 	
 				
@@ -54,17 +55,88 @@ public class TestAdhocTicket {
 	}
 	
 	@Test
-	public void testgetBarcode() {
+	public String testgetBarcode() {
 		
 		assertTrue(ibarcode instanceof ibarcode);
 		assertEquals(ibarcode.getName(), barcode);
 	}
-			@Test
-	public void testgetCarparkId() {
+	
+	@Test
+	public String testgetCarparkId() {
 		assertTrue(iCarpark instanceof ICarpark);
 		assertEquals(iCarpark.getName(), carparkId);
 	
 	}
+	
+	@Test
+	public Int testgetTicketNo() {
+		assertTrue(igetTicketNo instanceof igetTicketNo);
+		assertEquals(igetTicketNo.getName(), getTicketNo);
+	
+	}
+	
+	@Before
+	public void setUpenter() {
+		entryDateTime = mock(ientryDateTime,class);
+		STATE.CURRENT = mock(istate_, class);
+		
+	}
+	
+	@Test
+	public Int testgetEntryDateTime() {
+		assertTrue(ientryDateTime instanceof ientryDateTime);
+		assertEquals(ientryDateTime.getName(), entryDateTime);
+	
+	}
+	
+	@Test
+	public Int testgetEntryDateTime() {
+		assertTrue(ientryDateTime instanceof ientryDateTime);
+		assertEquals(ientryDateTime.getName(), entryDateTime);
+	
+	}
+
+	
+	
+	@before
+	public void setpay() {
+		
+		paidDateTime = mock(ipaidDateTime,class);
+		STATE.PAID = mock(iPAID, class);
+		
+	}
+	
+	@Test
+	public Int testgetPaidDateTime() {
+		assertTrue(igetPaidDateTime instanceof igetPaidDateTime);
+		assertEquals(igetPaidDateTime.getName(), getPaidDateTime);
+	
+	}
+	
+	@Test
+	public Int testgetCharge() {
+		assertTrue(igetCharge instanceof igetCharge);
+		assertEquals(igetCharge.getName(), getCharge);
+	
+	}
+	
+	@Test
+	public Int testisCurrent() {
+		assertTrue(igetCharge instanceof igetCharge);
+		assertEquals(igetCharge.getName(), getCharge);
+	
+	}
+
+
+	@Test
+	public long testgetExitDateTime() {
+		assertTrue(iexitDateTime instanceof iexitDateTime);
+		assertEquals(iexitDateTime.getName(), exitDateTime);
+	
+	}
+	
+	
+
 	
 
 
