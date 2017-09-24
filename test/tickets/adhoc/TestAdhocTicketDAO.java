ff33764523c9c840a426b886c6d300f7e17590bc
Sankaja <sugeeg@gmail.com>
2017-09-24 20:15:36 +1000

    ticket adhoc
    ticket adhoc

--- test/tickets/adhoc/TestAdhocTicketDAO.java
+++ test/tickets/adhoc/TestAdhocTicketDAO.java
@@ -1,5 +1,21 @@
 package test.tickets.adhoc;
 
-public class TestAdhocTicketDAO {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class TestAdhocTicketDAO {
+	
+	private Map<String, IAdhocTicket> currentTickets;
+	private IAdhocTicketFactory adhocTicketFactory_;
+	private int currentTicketNo;
+
+	
+		@Before
+	public void testAdhocTicketDAO() {
+		adhocTicketFactory_ = mock(iadhocTicketFactory_.class);
+		currentTickets = new HashMap<>();
+	}
 }
